c78d4c1498f8cfbf108f0065b6e9e7fc78cd4efa
Martin Addante <martin.addante@gmail.com>
2018-03-10 15:26:28 -0500

    job board minus initial lines that link to javascript links

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,38 @@
+class JobsController < ApplicationController
+
+  def create
+    p "In the create method!!!!!"
+    job = Job.create(job_params)
+    p job
+    redirect_to jobs_path
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+  end
+
+  def index
+    @jobs = Job.all
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+    redirect_to jobs_path
+  end
+
+  private
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
